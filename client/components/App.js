b969acd266aadf7daccd4a7514f99ccb4a515792
elliotaplant <elliotaplant@gmail.com>
2016-01-27 20:59:33 -0800

    break everything, working on defaulting the initial state by searching youtube for puppies

--- client/components/App.js
+++ client/components/App.js
@@ -2,15 +2,34 @@
 class App extends React.Component{
   constructor(props) {
     super(props);
-    var video = this.props.videoData[0];
+    var firstVideo = props.videoData[0];
+
     this.state = {
       activeVideo:{
-        title: video.snippet.title,
-        description: video.snippet.description,
-        videoId: video.id.videoId
+        title: firstVideo.snippet.title,
+        description: firstVideo.snippet.description,
+        videoId: firstVideo.id.videoId
       },
-      videoList: window.exampleVideoData
+      videoList: props.videoData
     };
+  }
+
+  componentWillMount() {
+    console.log('componentWillMount');
+    var self = this;
+    window.searchYouTube('funny puppies', (data)=>{
+      console.log('into the search');
+      var videoData = data.results;
+      var firstVideo = videoData[0];
+      self.setState({
+        activeVideo:{
+          title: firstVideo.snippet.title,
+          description: firstVideo.snippet.description,
+          videoId: firstVideo.id.videoId
+        },
+        videoList: videoData
+      },console.log("I should have rendered but i'm just logging shit to the console"));
+    });
   }
 
   render(){
--- client/lib/searchYouTube.js
+++ client/lib/searchYouTube.js
@@ -1,19 +1,6 @@
 var searchYouTube = (options, callback) => {
-  // $.ajax({
-  //   url: 'https://www.googleapis.com/youtube/v3/search',
-  //   part: "snippet",
-  //   type: 'GET',
-  //   data: { 
-  //     q: options.trim().replace(/ /g,'+'),
-  //     key: window.YOUTUBE_API_KEY,
-  //   },
-  //   dataType:'json',
-  //   success: data => callback(data),
-  //   error: () => console.log('youtube request failed')
-  // });
-
-console.log(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${options.trim().replace(/ /g,'+')}&type=video&key=${window.YOUTUBE_API_KEY}`);
   return $.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${options.trim().replace(/ /g,'+')}&type=video&key=${window.YOUTUBE_API_KEY}`,
                 data => { window.exampleVideoData = data.items})
 };  
+
 window.searchYouTube = searchYouTube;
