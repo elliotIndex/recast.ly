dbc776dbfad0af0e95855bea64f179c3d614fc12
elliotaplant <elliotaplant@gmail.com>
2016-01-27 16:34:55 -0800

    Dynamically bind video data to video list
    Also refactor VideoListEntry to ES6 react class

--- client/components/VideoList.js
+++ client/components/VideoList.js
@@ -6,9 +6,14 @@ class VideoList extends React.Component{
 
     render() {
         var videos = [];
+        var snippet;
         for (var i = 0; i < this.props.videoData.length; i++) {
+            snippet = this.props.videoData[i].snippet;
             videos.push( <VideoListEntry 
-                            title={this.props.videoData[i].snippet.title} />);
+                            key={this.props.videoData[i].id.videoId}
+                            title={snippet.title}
+                            detail={snippet.description}
+                            src={snippet.thumbnails.default.url} />);
         }
         return (
           <div className="video-list media">{videos}</div>
--- client/components/VideoPlayer.js
+++ client/components/VideoPlayer.js
@@ -1,23 +1,24 @@
 
 class VideoPlayer extends React.Component{
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+      super(props);
+  }
 
-    render(){ 
-      return ( 
-        <div className="video-player">
-          <div className="embed-responsive embed-responsive-16by9">
-            <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" allowFullScreen></iframe>
-          </div>
-          <div className="video-player-details">
-            <h3>Video Title</h3>
-            <div>Video Description</div>
-          </div>
+  render(){ 
+    return ( 
+      <div className="video-player">
+        <div className="embed-responsive embed-responsive-16by9">
+          <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" allowFullScreen></iframe>
         </div>
-      )
-    }
+        <div className="video-player-details">
+          <h3>Video Title</h3>
+          <div>Video Description</div>
+        </div>
+      </div>
+    )
+  }
 }
+
 
 // var VideoPlayer = () => (
 //   <div className="video-player">
