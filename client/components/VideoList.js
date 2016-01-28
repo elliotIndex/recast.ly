48ac9ac984c771fc0884b986c969c4b5d98279d9
elliotaplant <elliotaplant@gmail.com>
2016-01-27 16:15:34 -0800

    refactored VideoList, VideoPlayer, into ES6 class format
    Bound exampleVideoData to the window
    Now displays correct number of videos

--- client/components/VideoList.js
+++ client/components/VideoList.js
@@ -1,15 +1,30 @@
-var VideoList = () => (
-  <div className="video-list media">
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-  </div>
-);
+
+class VideoList extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        var videos = [];
+        for (var i = 0; i < this.props.videoData.length; i++) {
+            videos.push( <VideoListEntry 
+                            title={this.props.videoData[i].snippet.title} />);
+        }
+        return (
+          <div className="video-list media">{videos}</div>
+        );
+    }
+}
+// var VideoList = () => {
+//     var videos = [];
+//     for (var i = 0; i < this.props.videoData.length; i++) {
+//         videos.push( <VideoListEntry 
+//             title={this.props.videoData[i].snippet.title}
+//         />);
+//     };
+//     return (
+//       <div className="video-list media">{videos}</div>
+//     );
+// }
 
 window.VideoList = VideoList;
