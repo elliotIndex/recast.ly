fa689a609af8ac1203a6af22bf46a14383e9a5db
elliotaplant <elliotaplant@gmail.com>
2016-01-28 15:38:49 -0800

    throttled search to 1500 ms

--- client/components/Search.js
+++ client/components/Search.js
@@ -1,19 +1,25 @@
 class Search extends React.Component{
   constructor(props){
     super(props);
+    this.throttledSearch = _.throttle(()=> {
+            window.searchYouTube(
+              $('.form-control').val(),   
+              (data) => this.props.modifyAppState(window.makeStateObject(data))
+            );
+          }, 1500);
   }
 
   render(){
     return(
       <div className="search-bar form-inline">
         <input className="form-control" type="text" 
-          onKeyUp={()=> {
-            console.log($('.form-control').val());
-            window.searchYouTube(
-              $('.form-control').val(),   
-              (data) => this.props.modifyAppState(window.makeStateObject(data))
-            );
-          }}
+          onKeyUp={ this.throttledSearch }
+            // ()=> {
+            // window.searchYouTube(
+            //   $('.form-control').val(),   
+            //   (data) => this.props.modifyAppState(window.makeStateObject(data))
+            // );
+          // }}
         />
         <button className="btn hidden-sm-down">
           <span className="glyphicon glyphicon-search"></span>
